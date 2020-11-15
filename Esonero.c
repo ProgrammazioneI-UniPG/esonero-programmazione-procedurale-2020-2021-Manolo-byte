d5db386912a498af4aaff2bafafa2ce967bdcdb5
Manolo-byte <manugi@outlook.it>
2020-11-15 16:58:23 +0100

    Programma Aggiunto

--- Esonero.c
+++ Esonero.c
@@ -4,27 +4,27 @@
 #include <time.h>
 
 int main(){
-  int f=0, x=0, p=11, a=0, k=0; //Inizializzazione delle variabili
+  int f=0, x=0, p=0, a=0, k=0; //Inizializzazione delle variabili
   time_t tempo;
-  char ch;
-  char plaintext[129]; //Array per la stringa plaintext
-  char key[129]; //Array per la key
-  char encrypted[129]; //Array per la parola decriptata
-  char decrypted[129]; //Array per la parola decriptata
+  char ch, ch2;
+  char plaintext[128], key[128], encrypted[128], decrypted[128]; //Array di caratteri
   printf("Inserisci il plaintext: \n");
-  fgets(plaintext, 129, stdin); //Inserimento del plaintext
+  fgets(plaintext, 128, stdin); //Inserimento del plaintext
   p = strlen(plaintext); //Calcololo della lunghezza del plaintext
+  if(p>126){
+    while((ch = getchar()) != '\n' && ch != EOF); //Azzera il buffer
+  }
   printf("----Menù----\n");
   printf("Scegli 1 se vuoi inserire tu stesso la key\n");
   printf("Scegli 2 se vuoi generare randomicamente una key\n");
   scanf("%d", &a); //Scelta nel menù
-  while((ch = getchar()) != '\n' && ch != EOF); //Azzera il buffer
+  while((ch2 = getchar()) != '\n' && ch2 != EOF); //Azzera il buffer
   switch(a){
     case 1:
       while(f==0){
         if(k<p){
           printf("Inserisci la key, però deve essere maggiore o uguale al plaintext: \n");
-          fgets(key, 129, stdin); //Inserimento della chiave da utente
+          fgets(key, 128, stdin); //Inserimento della chiave da utente
           k = strlen(key); //Calcolo della lunghezza della key
         }
         else{
@@ -47,5 +47,5 @@ int main(){
   for(x=0; x!=p; x++){
     decrypted[x]=encrypted[x]^key[x]; //Calcolo dello XOR per la parola decriptata
   }
-  printf("Ecco la stringa decriptata: %s", decrypted); //Restituzione della parola decriptata
+  printf("Ecco la stringa decriptata: %s\n", decrypted); //Restituzione della parola decriptata
 }
